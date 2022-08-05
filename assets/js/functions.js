255219eba7399f1275dbc97505c0066ffb2716a7
Dc-Valerian <valerianpedro03@gmail.com>
2022-08-05 13:04:11 +0100

    first commit

---
+++ assets/js/functions.js
@@ -0,0 +1,42 @@
+function alabian() {
+    //content
+    alert("Valerian Pedro")
+}
+
+
+alabian();
+
+// let Valerian = prompt("Enter Your Name" );
+// let Valerian = prompt("Enter Your Name" );
+// let Valerian = prompt("Enter Your Name" );
+// document.write(Valerian);
+
+
+// const questions = [
+//     {
+//         question : "who is the president of russia\n (a)Buhari \n (b) puttin \n (C)Bidden \n ",
+//         correct :"b",
+
+//     },
+//     {
+//         question: "who is the best player \n (a)lewandoki \n (b) ronaldo \n (C)messi \n ",
+//         correct :"a",
+//     },
+//     {
+//         question : "1+2+3= \n (a)4 \n (b)5 \n (c)6 \n ",
+//         correct :"c",
+//     }
+// ]
+
+//console.log(questions[1].question2);
+
+//LOOP THROUGH AN ARRAY
+
+
+// for (let i = 0; i < questions.length; i++) {
+//     // console.log(questions[i]);
+
+//     let quest = questions[i];
+//     let myQuestion = prompt(quest.question) 
+    
+// }
